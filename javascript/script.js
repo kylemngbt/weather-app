712bc3cf15254cf18e161c6663ced98e5e102bcc
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-02 14:15:43 +0800

    feat: only display the weather details if the city name is valid

--- javascript/script.js
+++ javascript/script.js
@@ -9,32 +9,37 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-  let data = await response.json();
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    let data = await response.json();
 
-  document.querySelector(".city").textContent = data.name;
-  document.querySelector(".temp").textContent = `${Math.round(data.main.temp)}°C`;
-  document.querySelector(".humidity").textContent = `${data.main.humidity}%`;
-  document.querySelector(".wind").textContent = `${data.wind.speed} km/h`;
-
-  switch (data.weather[0].main) {
-    case "Clouds":
-      weatherIcon.src = "./images/clouds.png";
-      break;
-    case "Clear":
-      weatherIcon.src = "./images/clear.png";
-      break;
-    case "Rain":
-      weatherIcon.src = "./images/rain.png";
-      break;
-    case "Drizzle":
-      weatherIcon.src = "./images/drizzle.png";
-      break;
-    case "Mist":
-      weatherIcon.src = "./images/mist.png";
-      break;
+    document.querySelector(".city").textContent = data.name;
+    document.querySelector(".temp").textContent = `${Math.round(data.main.temp)}°C`;
+    document.querySelector(".humidity").textContent = `${data.main.humidity}%`;
+    document.querySelector(".wind").textContent = `${data.wind.speed} km/h`;
+  
+    switch (data.weather[0].main) {
+      case "Clouds":
+        weatherIcon.src = "./images/clouds.png";
+        break;
+      case "Clear":
+        weatherIcon.src = "./images/clear.png";
+        break;
+      case "Rain":
+        weatherIcon.src = "./images/rain.png";
+        break;
+      case "Drizzle":
+        weatherIcon.src = "./images/drizzle.png";
+        break;
+      case "Mist":
+        weatherIcon.src = "./images/mist.png";
+        break;
+    }
+  
+    document.querySelector(".weather").style.display = "block";
   }
-
-  document.querySelector(".weather").style.display = "block";
 }
 
 searchBtn.addEventListener("click", () => {
