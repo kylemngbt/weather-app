1afd1401d61d8f8b834ba197b6bf903986320e73
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-02 13:46:22 +0800

    build: create two references to an element

--- javascript/script.js
+++ javascript/script.js
@@ -2,10 +2,12 @@
 // https://api.openweathermap.org/data/2.5/weather?q=germany&appid=fb85986ca4be306e4ef21b120af88058&units=metric
 
 const apiKey = "fb85986ca4be306e4ef21b120af88058";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=manila";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
 
-async function checkWeather() {
-  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
   let data = await response.json();
 
   document.querySelector(".city").textContent = data.name;
