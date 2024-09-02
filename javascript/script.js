d364ab700da4cfbc99b043a243143def2929741d
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-02 13:31:08 +0800

    build: create the api variables

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+// Weather API
+// https://api.openweathermap.org/data/2.5/weather?q=germany&appid=fb85986ca4be306e4ef21b120af88058&units=metric
+
+const apiKey = "fb85986ca4be306e4ef21b120af88058";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+
+
